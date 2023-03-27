834a5de617e3c4dfe719a3a13e18553e9d7b8458
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-03-27 13:09:45 -0700

    added cart section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Header from './compononents/Header/Header'
+import Shop from './compononents/Shop/Shop'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
