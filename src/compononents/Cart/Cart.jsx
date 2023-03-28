9df1e23ad3cbe8ebacf3be6616e2f913b80ef7b4
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-03-28 04:38:09 -0700

    added database

--- src/compononents/Cart/Cart.jsx
+++ src/compononents/Cart/Cart.jsx
@@ -3,21 +3,22 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
     // const {cart}=props;
-    let total=0;
+    let totalPrice=0;
     let totalShipping=0;
     for(const product of cart){
-        total=total+product.price;
+        totalPrice=totalPrice+product.price;
         totalShipping=totalShipping+product.shipping;
     }
-    const tax=total*7/100;
+    const tax=totalPrice*7/100;
+    const grandTotal=totalPrice+totalShipping+tax;
     return (
         <div className='cart'>
             <h2>Order Summery</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total}</p>
+            <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
-            <p>Tax: ${tax}</p>
-            <h6>Grand Total:</h6>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
