d322ee50d323a64c462c3e9fc76178a6a0ab649a
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-03-28 03:37:26 -0700

    added cart update

--- src/compononents/Cart/Cart.jsx
+++ src/compononents/Cart/Cart.jsx
@@ -4,16 +4,19 @@ import './Cart.css';
 const Cart = ({cart}) => {
     // const {cart}=props;
     let total=0;
+    let totalShipping=0;
     for(const product of cart){
         total=total+product.price;
+        totalShipping=totalShipping+product.shipping;
     }
+    const tax=total*7/100;
     return (
         <div className='cart'>
             <h2>Order Summery</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping Charge: </p>
-            <p>Tax:</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
             <h6>Grand Total:</h6>
         </div>
     );
